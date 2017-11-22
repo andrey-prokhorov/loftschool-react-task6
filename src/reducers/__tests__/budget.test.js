869bdd2ce0d491bf32722ba660cd7bd827417f7a
Andrey Prokhorov <andrey@ezy.se>
2017-11-23 00:45:18 +0100

    :factory: Renamed variables in tests

--- src/reducers/__tests__/budget.test.js
+++ src/reducers/__tests__/budget.test.js
@@ -1,20 +1,23 @@
-import budget from '../budget';
-import {MOVE_ORDER_TO_CUSTOMER} from '../../actions/farmTypes';
-import {CREATE_ORDER, MOVE_ORDER_TO_FARM} from '../../actions/marketTypes';
+import budget from "../budget";
+import {
+  MOVE_ORDER_TO_CUSTOMER,
+  CREATE_ORDER,
+  MOVE_ORDER_TO_FARM
+} from "../../constants/actionTypes";
 
-describe('reducer budget', () => {
-  it('экшен с типом MOVE_ORDER_TO_CUSTOMER увеличивает deliveryExpanse на 20', () => {
-    const next = budget({}, {type: MOVE_ORDER_TO_CUSTOMER});
-    expect(next.deliveryExpanse).toEqual(20);
+describe("reducer budget", () => {
+  it("экшен с типом MOVE_ORDER_TO_CUSTOMER увеличивает deliveryExpanses на 20", () => {
+    const next = budget({}, { type: MOVE_ORDER_TO_CUSTOMER });
+    expect(next.deliveryExpanses).toEqual(20);
   });
 
-  it('экшен с типом CREATE_ORDER увеличивает profit на action.payload.price', () => {
-    const next = budget({}, {type: CREATE_ORDER, payload: {price: 111}});
+  it("экшен с типом CREATE_ORDER увеличивает profit на action.payload.price", () => {
+    const next = budget({}, { type: CREATE_ORDER, payload: { price: 111 } });
     expect(next.profit).toEqual(111);
   });
 
-  it('экшен с типом MOVE_ORDER_TO_FARM увеличивает farmExpanse на 100', () => {
-    const next = budget({}, {type: MOVE_ORDER_TO_FARM});
-    expect(next.farmExpanse).toEqual(100);
+  it("экшен с типом MOVE_ORDER_TO_FARM увеличивает farmExpanses на 100", () => {
+    const next = budget({}, { type: MOVE_ORDER_TO_FARM });
+    expect(next.farmExpanses).toEqual(100);
   });
 });
